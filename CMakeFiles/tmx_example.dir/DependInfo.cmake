f0fd51ee9de034f9a95e0352d63bcd1e30a51cf9
Niamul Abid <iamzihan055@gmail.com>
2025-09-26 12:14:04 +0600

    new

---
+++ CMakeFiles/tmx_example.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/abid/Desktop/#codes/#Raylib game project/boss.c" "CMakeFiles/tmx_example.dir/boss.c.o" "gcc" "CMakeFiles/tmx_example.dir/boss.c.o.d"
+  "/Users/abid/Desktop/#codes/#Raylib game project/character.c" "CMakeFiles/tmx_example.dir/character.c.o" "gcc" "CMakeFiles/tmx_example.dir/character.c.o.d"
+  "/Users/abid/Desktop/#codes/#Raylib game project/game.c" "CMakeFiles/tmx_example.dir/game.c.o" "gcc" "CMakeFiles/tmx_example.dir/game.c.o.d"
+  "/Users/abid/Desktop/#codes/#Raylib game project/main.c" "CMakeFiles/tmx_example.dir/main.c.o" "gcc" "CMakeFiles/tmx_example.dir/main.c.o.d"
+  "/Users/abid/Desktop/#codes/#Raylib game project/map_renderer.c" "CMakeFiles/tmx_example.dir/map_renderer.c.o" "gcc" "CMakeFiles/tmx_example.dir/map_renderer.c.o.d"
+  "/Users/abid/Desktop/#codes/#Raylib game project/projectile.c" "CMakeFiles/tmx_example.dir/projectile.c.o" "gcc" "CMakeFiles/tmx_example.dir/projectile.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/tmx_example.dir/interface.c.o.d
@@ -0,0 +1,12 @@
+CMakeFiles/tmx_example.dir/interface.c.o: \
+  /Users/abid/Desktop/\#codes/\#Raylib\ game\ project/interface.c \
+  /Users/abid/Desktop/\#codes/\#Raylib\ game\ project/interface.h \
+  /opt/homebrew/include/raylib.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/17/include/stdarg.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/17/include/__stdarg_header_macro.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/17/include/__stdarg___gnuc_va_list.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/17/include/__stdarg_va_list.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/17/include/__stdarg_va_arg.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/17/include/__stdarg___va_copy.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/17/include/__stdarg_va_copy.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/17/include/stdbool.h
